50fc1bf5e1d2188b3d602ad8f0eeb4139a07812a
JWo124 <moskit48@gmail.com>
2024-06-12 19:38:34 +0200

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,12 @@
-# Code Example
+#!/bin/bash
+# Code Sample 
+# calculates simple annual interest given principa
+
+   echo "Your principal:"
+   read a
+   echo "Anual rate of interest:"
+   read b
+   r=`expr $a \* $b / 100`
+   echo "Hier your anual interest: "
+   echo $r
+   echo "Done"
